f8a7e7640664ac6e584c41ffbf39da2ae276d590
Bahman Movaqar <Bahman@BahmanM.com>
2025-07-21 14:15:10 -0700

    Implement Visitor pattern for execution

--- src/BahmanM.Flow/FlowExtensions.cs
+++ src/BahmanM.Flow/FlowExtensions.cs
@@ -6,11 +6,11 @@ public static class FlowExtensions
         new DoOnSuccessNode<T>(flow, action);
 
     public static IFlow<T> DoOnSuccess<T>(this IFlow<T> flow, Func<T, Task> asyncAction) =>
-        new AsyncDoOnSuccessNode<T>(flow, asyncAction);
+        new AsyncDoOnSuccessFlow<T>(flow, asyncAction);
 
     public static IFlow<TOut> Select<TIn, TOut>(this IFlow<TIn> flow, Func<TIn, TOut> operation) =>
-        new SelectNode<TIn, TOut>(flow, operation);
+        new SelectFlow<TIn, TOut>(flow, operation);
 
     public static IFlow<TOut> Select<TIn, TOut>(this IFlow<TIn> flow, Func<TIn, Task<TOut>> asyncOperation) =>
-        new AsyncSelectNode<TIn, TOut>(flow, asyncOperation);
+        new AsyncSelectFlow<TIn, TOut>(flow, asyncOperation);
 }
