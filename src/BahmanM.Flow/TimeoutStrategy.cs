b8261d723847566592f985b4cfca0730d95c75bc
Bahman Movaqar <Bahman@BahmanM.com>
2025-07-24 02:35:41 -0700

    [#64] Add failing test for WithBehaviour on failed flow

--- src/BahmanM.Flow/TimeoutStrategy.cs
+++ src/BahmanM.Flow/TimeoutStrategy.cs
@@ -13,16 +13,27 @@ internal class TimeoutStrategy(TimeSpan duration) : IBehaviourStrategy
 
     public IFlow<T> ApplyTo<T>(AsyncCreateNode<T> node)
     {
-        Func<Task<T>> newOperation = async () => await node.Operation().WaitAsync(duration);
-        return new AsyncCreateNode<T>(() => newOperation());
+        Func<Task<T>> newOperation = () => node.Operation().WaitAsync(duration);
+        return new AsyncCreateNode<T>(newOperation);
     }
 
-    public IFlow<T> ApplyTo<T>(DoOnSuccessNode<T> node) => node;
-    public IFlow<T> ApplyTo<T>(AsyncDoOnSuccessNode<T> node) => node;
-    public IFlow<T> ApplyTo<T>(DoOnFailureNode<T> node) => node;
-    public IFlow<T> ApplyTo<T>(AsyncDoOnFailureNode<T> node) => node;
-    public IFlow<TOut> ApplyTo<TIn, TOut>(SelectNode<TIn, TOut> node) => node;
-    public IFlow<TOut> ApplyTo<TIn, TOut>(AsyncSelectNode<TIn, TOut> node) => node;
+    public IFlow<T> ApplyTo<T>(DoOnSuccessNode<T> node) =>
+        node with { Upstream = ((IFlowNode<T>)node.Upstream).Apply(this) };
+
+    public IFlow<T> ApplyTo<T>(AsyncDoOnSuccessNode<T> node) =>
+        node with { Upstream = ((IFlowNode<T>)node.Upstream).Apply(this) };
+
+    public IFlow<T> ApplyTo<T>(DoOnFailureNode<T> node) =>
+        node with { Upstream = ((IFlowNode<T>)node.Upstream).Apply(this) };
+
+    public IFlow<T> ApplyTo<T>(AsyncDoOnFailureNode<T> node) =>
+        node with { Upstream = ((IFlowNode<T>)node.Upstream).Apply(this) };
+
+    public IFlow<TOut> ApplyTo<TIn, TOut>(SelectNode<TIn, TOut> node) =>
+        node with { Upstream = ((IFlowNode<TIn>)node.Upstream).Apply(this) };
+
+    public IFlow<TOut> ApplyTo<TIn, TOut>(AsyncSelectNode<TIn, TOut> node) =>
+        node with { Upstream = ((IFlowNode<TIn>)node.Upstream).Apply(this) };
 
     public IFlow<TOut> ApplyTo<TIn, TOut>(ChainNode<TIn, TOut> node)
     {
@@ -41,17 +52,5 @@ internal class TimeoutStrategy(TimeSpan duration) : IBehaviourStrategy
             return ((IFlowNode<TOut>)nextFlow).Apply(this);
         };
         return node with { Operation = newOperation };
-    }
-
-    public IFlow<T[]> ApplyTo<T>(AllNode<T> node)
-    {
-        Func<Task<T[]>> newOperation = () => FlowEngine.ExecuteAsync(node).WaitAsync(duration).Unwrap();
-        return new AsyncCreateNode<T[]>(newOperation);
-    }
-
-    public IFlow<T> ApplyTo<T>(AnyNode<T> node)
-    {
-        Func<Task<T>> newOperation = () => FlowEngine.ExecuteAsync(node).WaitAsync(duration).Unwrap();
-        return new AsyncCreateNode<T>(newOperation);
     }
 }
