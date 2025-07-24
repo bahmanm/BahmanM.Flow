3cfeeaddbdcea61f503aa06ca935c367ec924611
Bahman Movaqar <bahman@bahmanm.com>
2025-07-24 16:28:23 -0700

    [#3, #4] Implement Flow.All and Flow.Any concurrency operators (#69)

--- src/BahmanM.Flow/FlowEngine.cs
+++ src/BahmanM.Flow/FlowEngine.cs
@@ -79,15 +79,8 @@ public class FlowEngine
 
         if (upstreamOutcome is Failure<T> failure)
         {
-            try
-            {
-                node.Action(failure.Exception);
-            }
-            catch
-            {
-                // Per the contract, if the action throws, we ignore the new exception
-                // and propagate the original failure.
-            }
+            try { node.Action(failure.Exception); }
+            catch { /* Ignore */ }
         }
 
         return upstreamOutcome;
@@ -99,15 +92,8 @@ public class FlowEngine
 
         if (upstreamOutcome is Failure<T> failure)
         {
-            try
-            {
-                await node.AsyncAction(failure.Exception);
-            }
-            catch
-            {
-                // Per the contract, if the action throws, we ignore the new exception
-                // and propagate the original failure.
-            }
+            try { await node.AsyncAction(failure.Exception); }
+            catch { /* Ignore */ }
         }
 
         return upstreamOutcome;
